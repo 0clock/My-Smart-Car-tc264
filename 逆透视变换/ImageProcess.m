31164faf3c7a194535c142cae448fe3821bb0272
PLZ <2436478810@qq.com>
2022-02-27 19:58:37 +0800

    K-means改为3分类，便于识别强光

--- 逆透视变换/ImageProcess.m
+++ 逆透视变换/ImageProcess.m
@@ -1,9 +1,20 @@
 clc,clear,close all;
-A = imread('1064.7.pgm');
-B = ImageProcessing(A, 30, 20, 6, 0.005, 2);
+A = imread('1688.64.pgm');
+B = ImageProcessing(A, 40, 27.89191, 5.915322, 0.1, 2);
 
-subplot(1 ,2 ,1),imshow(B,[0,256]);
+
+
+tic
+value = Kmeans(A)
+toc
+
+
+
+subplot(2 ,2 ,1),imshow(A,[0,256]);
 title('原始图')
-subplot(1 ,2 ,2),imshow(C,[0,256]);
+subplot(2 ,2 ,2),imshow(B,[0,256]);
 title('逆透视变换图')
-
+subplot(2 ,2 ,3),imshow(A>value,[0,1]);
+title('原始图二值化')
+subplot(2 ,2 ,4),imshow(B>value,[0,1]);
+title('逆透视变换图二值化')
--- 逆透视变换/Kmeans.m
+++ 逆透视变换/Kmeans.m
@@ -2,25 +2,32 @@ function [value] = Kmeans(image)
     image = double(image);
     m1 = 85;
     m2 = 171;
+    m3 = 255;
     new_m1 = 0;
     new_m2 = 0;
+    new_m3 = 0;
     maxtimes = 2;
     for time = 1:maxtimes
         cnt_m1 = 0;
         cnt_m2 = 0;
+        cnt_m3 = 0;
         for i = 1:size(image,1)
             for j = 1:size(image,2)            
-                if image(i,j)>0.5.*(m1+m2)
+                if image(i,j)>0.5.*(m1+m2) && image(i,j)<0.5.*(m2+m3)
                     cnt_m2 = cnt_m2 + 1;
                     new_m2 = (cnt_m2-1)./cnt_m2.*new_m2 + image(i,j)./cnt_m2;
-                else
+                elseif image(i,j)<=0.5.*(m1+m2)
                     cnt_m1 = cnt_m1 + 1;
                     new_m1 = (cnt_m1-1)./cnt_m1.*new_m1 + image(i,j)./cnt_m1;
+                else
+                    cnt_m3 = cnt_m3 + 1;
+                    new_m3 = (cnt_m3-1)./cnt_m3.*new_m3 + image(i,j)./cnt_m3;    
                 end
             end
         end
         m1 = new_m1;
         m2 = new_m2;
+        m3 = new_m3;
     end
     value = uint8(0.5.*(m1+m2));
 end
