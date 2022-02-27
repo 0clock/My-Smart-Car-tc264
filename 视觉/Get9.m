b531abd85208510520bed9ec118f3dc5bca1c4b0
PLZ <2436478810@qq.com>
2022-02-27 22:28:37 +0800

    改为9个特征值

--- 视觉/Get9.m
+++ 视觉/Get9.m
@@ -1,4 +1,4 @@
-function arg = Get6(image)
+function arg = Get9(image)
     width = size(image,2);
     height = size(image,1);
     col_edge(1) = 1;
@@ -6,18 +6,19 @@ function arg = Get6(image)
     col_edge(3) = 2*col_edge(2);
     col_edge(4) = ceil(width);
     row_edge(1) = 1;
-    row_edge(2) = ceil(height/2);
-    row_edge(3) = ceil(height);
+    row_edge(2) = ceil(height/3);
+    row_edge(3) = 2*row_edge(2);
+    row_edge(4) = ceil(height);
     
-    white_cnt = zeros(1,6);
-    black_cnt = zeros(1,6);
+    white_cnt = zeros(1,9);
+    black_cnt = zeros(1,9);
     for i = 1:height
         for j = 1:width
             white_cnt(3*ceil(i/row_edge(2)) + ceil(j/col_edge(2)) -3) = (image(i,j)==1) + white_cnt(3*ceil(i/row_edge(2)) + ceil(j/col_edge(2)) -3);
             black_cnt(3*ceil(i/row_edge(2)) + ceil(j/col_edge(2)) -3) = (image(i,j)==0) + black_cnt(3*ceil(i/row_edge(2)) + ceil(j/col_edge(2)) -3);
         end
     end
-    for i = 1:6
+    for i = 1:9
        if (white_cnt(i)+black_cnt(i)) == 0
            arg(i)=0;
        else
--- 视觉/ImageProcess.m
+++ 视觉/ImageProcess.m
@@ -1,12 +1,12 @@
 clc,clear,close all;
 
 %读图片，然后二值+逆透视
-A = imread('200.pgm');
+A = imread('1064.7.pgm');
 value = Kmeans(A)
 B = ImageProcessing(A>value, 40, 27.89191, 5.915322, 0.1, 2);
-%求出图片的6个特征值
-arg = Get6(B)
-%6个特征值写入excel表格（还没写）
+%求出图片的9个特征值
+arg = Get9(B)
+%9个特征值写入excel表格（还没写）
 %然后循环上述部分，直到某个赛道类别的图片集全部结束
 
 %PS：一个赛道类别弄一个特征值excel表格
