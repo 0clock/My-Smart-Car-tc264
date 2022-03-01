02fbb8cbf4b9fcf29e3aa9f768b295815ba50cf7
PLZ <2436478810@qq.com>
2022-03-01 18:14:45 +0800

    更新了12种赛道类型的分类MATLAB代码（Classification.m）和对应的使用代码（ImageProcess.m）

--- 视觉/ImageProcess.m
+++ 视觉/ImageProcess.m
@@ -1,18 +1,14 @@
 clc,clear,close all;
 
 %读图片，然后二值+逆透视
-A = imread('1064.7.pgm');
-value = Kmeans(A)
+A = imread('151.pgm');
+value = Kmeans(A);
 B = ImageProcessing(A>value, 40, 27.89191, 5.915322, 0.1, 2);
-%求出图片的9个特征值
-arg = Get9(B)
-%9个特征值写入excel表格（还没写）
-%然后循环上述部分，直到某个赛道类别的图片集全部结束
-
-%PS：一个赛道类别弄一个特征值excel表格
+%识别类型
+class_Name = Classification(B);
 
 
 subplot(1 ,2 ,1),imshow(A,[0,256]);
 title('原始图')
 subplot(1 ,2 ,2),imshow(B,[0,1]);
-title('逆透视变换图二值化')
+title(class_Name)
