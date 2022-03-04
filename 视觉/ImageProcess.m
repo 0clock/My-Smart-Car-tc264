9b3ad7bf4f283c3636123268c7f8375c374067fe
PLZ <2436478810@qq.com>
2022-03-04 15:46:31 +0800

    MATLAB最新文件

--- 视觉/ImageProcess.m
+++ 视觉/ImageProcess.m
@@ -3,7 +3,7 @@ clc,clear,close all;
 %读图片，然后二值+逆透视
 A = imread('1170.pgm');
 value = Kmeans(A);
-B = ImageProcessing(A>value, 40, 27.89191, 5.915322, 0.1, 2);
+B = ImageProcessing(A(:,1:end-1)>value, 40, 27.89191, 5.915322, 0.1, 2);
 %识别类型
 if CheckStraightV2(B) == 1
     class_Name = '直道';
@@ -17,5 +17,5 @@ subplot(1 ,3 ,1),imshow(A,[0,256]);
 title('原始图')
 subplot(1 ,3 ,2),imshow(B,[0,1]);
 title(class_Name)
-subplot(1 ,3 ,3),imshow(C,[0,3]);
+subplot(1 ,3 ,3),imshow(C,[0,4]);
 title(class_Name)
